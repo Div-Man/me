22e6eb943d0d012b2037a75eb1122fc008afae08
Kaharov Dima <dkakharov@yandex.ru>
2017-03-22 10:27:08 +0300

    functions for dz-5

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+function getData(){
+	$data = '[{
+	  "firstName": "Иван",
+	  "lastName": "Иванов",
+	  "address": "г.Москва, ул. Алиева,2",
+	  "phoneNumber": "812 143-2534"
+	}, {
+	  "firstName": "Вася",
+	  "lastName": "Пупкин",
+	  "address": "г.Воронеж, ул. Пушкина,11",
+	  "phoneNumber": "812 123-1234"
+	}, {
+	  "firstName": "Сергей",
+	  "lastName": "Попов",
+	  "address": "г.Пермь, ул. Савина,30",
+	  "phoneNumber": "812 321-1444"
+	}]';
+	
+	return $data;
+}
